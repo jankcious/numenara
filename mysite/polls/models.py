597a7417e5ac336cb4ad175fe24fa435bf9acf0f
Jason Becker <jason.becker86@gmail.com>
2018-02-04 13:12:03 -0600

    complete through part 6

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,8 +1,7 @@
+# Create your models here.
 from django.db import models
 import datetime
 from django.utils import timezone
-
-# Create your models here.
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -10,7 +9,8 @@ class Question(models.Model):
     def __str__(self):
         return self.question_text
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
